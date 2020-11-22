f8531f95813582724820320d4418bc99ec343feb
dgash2201 <73651825+dgash2201@users.noreply.github.com>
2020-11-22 17:18:28 +0300

    Delete imports.sh

--- imports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "@import url(../vendor/normalize.css);" > ./pages/index.css
-
-for str in $(find blocks -name *.css)
-do
-	echo "@import url(../$str);" >> ./pages/index.css
-done
