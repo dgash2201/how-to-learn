9998523fe5d4fb587a165c3a7e23fd599e1f9e06
Danil Gashigullin <dgash2201@yandex.ru>
2020-11-22 17:16:37 +0300

    Исправил недочёты

---
+++ imports.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "@import url(../vendor/normalize.css);" > ./pages/index.css
+
+for str in $(find blocks -name *.css)
+do
+	echo "@import url(../$str);" >> ./pages/index.css
+done
